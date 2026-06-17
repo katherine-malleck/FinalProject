a33b1e25a28e35e608859a12765fd20a063ad113
katherine <katherine.malleck@stu.ocsb.ca>
2026-06-17 09:03:44 -0400

    added comments

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Katherine
+ * Created on: Jun 2026
+ * This program will make it so when you press a button connected to pin 0 it will show happy face
 */
 
-basic.showString('Hello, World!')
+// set pin 0 to 1
+pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
+basic.forever(function () {
+    // if the P0 is connected to ground
+    if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.clearScreen()
+    }
+})
+
